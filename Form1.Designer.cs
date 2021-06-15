6a381779f5663d76044e2cf67e25a480ca03c8d2
cyjfox <cyjfox@hotmail.com>
2021-06-15 10:40:44 +0800

    初始提交
    Signed-off-by: cyjfox <cyjfox@hotmail.com>

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SerialPortAssistance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 921600;
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.ReadBufferSize = 40960;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(691, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(604, 1);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(357, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(691, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1200, 1000);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(967, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1274, 1061);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Threading;
+
+namespace SerialPortAssistance
+{
+    public partial class Form1 : Form
+    {
+        static FileStream fileStream;
+        static uint count = 0;
+        static int lastChannelNumber = 0;
+        //static uint 
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            fileStream = File.Create(@"D:\workspace\SerialPortAssistance\data.txt");
+            //count = 0;
+            System.Timers.Timer t = new System.Timers.Timer(100000);//实例化Timer类，设置间隔时间为10000毫秒；
+
+            t.Elapsed += new System.Timers.ElapsedEventHandler(timeout);//到达时间的时候执行事件；
+
+            t.AutoReset = false;//设置是执行一次（false）还是一直执行(true)；
+
+            //t.Enabled = true;//是否执行System.Timers.Timer.Elapsed事件；
+            serialPort1.Open();
+
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string str = serialPort1.ReadExisting();     //字符串方式读
+            //serialPort1.read
+            textBox1.AppendText(str);//添加内容
+            byte[] buf = System.Text.Encoding.Default.GetBytes(str);
+            fileStream.Write(buf, 0, buf.Length);
+
+            //如果格式正确
+            if (buf[8] == '\n')
+            {
+                int channelNumber = buf[7] & 0x07;
+                if (channelNumber == lastChannelNumber)
+                {
+                    int value = Int32.Parse()
+                }
+
+                lastChannelNumber++;
+                if (lastChannelNumber >= 4)
+                {
+                    lastChannelNumber = 0;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            fileStream.Close();
+        }
+
+        
+
+ 
+
+        public void timeout(object source, System.Timers.ElapsedEventArgs e)
+        {
+            button2_Click(null, null);
+            FileStream fs = File.Open(@"D:\workspace\SerialPortAssistance\data.txt", FileMode.Open);
+            //while (fs.)
+            byte[] buf = new byte[16];
+            int i = 0;
+            int num;
+            int channelNumber = -1;
+            int errorCount = 0;
+            do
+            {
+                num = fs.Read(buf, 0, 9);
+                i++;
+                if (buf[8] != '\n')
+                {
+                    MessageBox.Show("非法格式的数据");
+                }
+                int channel = buf[7] & 0x07;
+                if (channelNumber == -1)
+                {
+                    channelNumber = channel;
+                }
+                if (channelNumber != channel)
+                {
+                    //MessageBox.Show("数据同步出错");
+                    errorCount++;
+                }
+
+                channelNumber++;
+                if (channelNumber >=4)
+                {
+                    channelNumber = 0;
+                }
+            } while (num >= 9);
+
+            MessageBox.Show("total number of samples is : " + i);
+            MessageBox.Show("error count is : " + errorCount);
+            MessageBox.Show("OK!");
+
+        }
+
+        private void draw()
+        {
+            Graphics graphics = Graphics.FromHwnd(pictureBox1.Handle);
+            graphics.DrawEllipse(Pens.Red, new Rectangle(20, 780, 200, 200));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            draw();
+        }
+    }
+
+    
+}
